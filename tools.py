2145a1e45ed71437293bed40334e273e6c283a3c
Adridu92 <adrien.resseguier@gmail.com>
2025-10-09 12:08:57 +0200

    adding text

--- tools.py
+++ tools.py
@@ -1,3 +1,12 @@
 quitting words: ["quit","ciao","byebye","au revor","adios"]
 def user_continues () :
 
+def message_de_bienvenue():
+    message_de_bienvenue = Welcome 
+    return(message_de_bienvenue)
+
+def demande_id () :
+    demande_id =  imput ("Entrez votre id")
+   
+def verifier_pin () :
+    verifier_pin = 
