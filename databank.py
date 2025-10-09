ad48fb2d29d88f7986dfef9c4033f7f010e82453
Estesan <estebansolanille169@gmail.com>
2025-10-09 11:47:02 +0200

    adding users

--- databank.py
+++ databank.py
@@ -4,3 +4,25 @@
 "solde" : 999999
 "depots" : [{"montant":10000, "date":05-10-2025}, \
             {"montant":12000, "date":02-10-2025}]
+"retraits":[]
+1033:
+"nom" : Goataldo,
+"prenom" : Christian,
+"solde" : 10023
+"depots" : [{"montant":1300, "date":03-10-2025}, \
+            {"montant":1200, "date":02-10-2025}]
+"retraits":[{"montant":200, "date":04-09-2025}]
+1023:
+"nom" : Raisingay,
+"prenom" : Hadrien,
+"solde" : 12
+"depots" : [{"montant":1, "date":03-10-2025}, \
+            {"montant":2, "date":01-10-2025}]
+"retraits":[{"montant": 1, "date":06-10-2025}]
+1043:
+"nom" : Moisi,
+"prenom" : Leon,
+"solde" : 0
+"depots" : [{"montant":1, "date":03-10-2025}, \
+            {"montant":1, "date":02-10-2025}]
+"retraits":[{"montant":2,"date":04-10-2025}]
