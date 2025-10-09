c6cade2b06730cfc83767bc7c18cb8b618c03749
Estesan <estebansolanille169@gmail.com>
2025-10-09 12:08:18 +0200

    adding users

--- databank.py
+++ databank.py
@@ -17,12 +17,37 @@
 "prenom" : Hadrien,
 "solde" : 12
 "depots" : [{"montant":1, "date":03-10-2025}, \
-            {"montant":2, "date":01-10-2025}]
+            {"montant":2, "date":01-10-2025}],
 "retraits":[{"montant": 1, "date":06-10-2025}]
 1043:
 "nom" : Moisi,
 "prenom" : Leon,
 "solde" : 0
 "depots" : [{"montant":1, "date":03-10-2025}, \
-            {"montant":1, "date":02-10-2025}]
+            {"montant":1, "date":02-10-2025}],
 "retraits":[{"montant":2,"date":04-10-2025}]
+1073:
+"nom" : Pêcheur,
+"prenom" : Marin,
+"solde" : 1200,
+"depots" : [{"montant":700, "date":"01-10-2025"},\
+             {"montant":600, "date":"03-10-2025"}],
+"retraits" : [{"montant":300, "date":"05-10-2025"}]
+1063:
+"nom" : clenchetasoeur,
+"prenom" : Jean,
+"solde" : 8000,
+"depots" : [{"montant":500, "date":"02-10-2025"}],
+"retraits" : [{"montant":150, "date":"04-10-2025"}]
+1093:
+"nom" : Boulard,
+"prenom" : Alban,
+"solde" : 1500,
+"depots" : [{"montant":100, "date":"03-10-2025"}],
+"retraits" : [{"montant":75, "date":"04-10-2025"}]
+1193:
+"nom" : jinping,
+"prenom" : Xi,
+"solde" : 240000000,
+"depots" : [{"montant":100000, "date":"03-10-2025"}],
+"retraits" : [{"montant":75000, "date":"04-10-2025"}]
