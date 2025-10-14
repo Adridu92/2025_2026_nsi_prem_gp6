ca7ea2f219d7e6606b00b1c844fa5f6446f2345f
Estesan <estebansolanille169@gmail.com>
2025-10-14 16:09:44 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -5,11 +5,9 @@ def message_de_bienvenue():
     message_de_bienvenue = Welcome 
     return(message_de_bienvenue)
 
-def demande_id () :
+def entrez_code_pin () :
     demande_id =  imput ("Entrez votre id")
    
-def verifier_pin () :
-    tentative=0
-    while tentatives < 3:
-         code_saisi = input("Entrez votre code PIN : ")
+def pin_good():
+    pin=
          
