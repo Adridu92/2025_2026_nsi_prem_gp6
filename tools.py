1e208eb7bc9a37f53c6b241236171ff95b750b36
Adridu92 <adrien.resseguier@gmail.com>
2025-10-16 11:52:49 +0200

    adding code

--- tools.py
+++ tools.py
@@ -1,13 +1,37 @@
-quitting words: ["quit","ciao","byebye","au revor","adios"]
-def user_continues () :
+def pin_good():
+    pin = [1013, 1023, 1033, 1043, 1063, 1073, 1093, 1193 ]
+         
+quitting_words = ["quit", "ciao", "byebye", "au revoir", "adios"]
+
+pins_valides = [1013, 1023, 1033, 1043, 1063, 1073, 1093, 1193]
 
 def message_de_bienvenue():
-    message_de_bienvenue = Welcome 
-    return(message_de_bienvenue)
+    print("Bonjour ! Bienvenue au distributeur automatique de billets.")
 
-def entrez_code_pin () :
-    demande_id =  imput ("Entrez votre id")
-   
-def pin_good():
-    pin = [1013,1023,1033;1043,1063,1073,1093,1193]
-         
+def entrer_code_pin():
+    while True:
+        demande_id = input("Entrez votre code PIN (ou tapez 'quit' pour quitter) : ")
+        
+        if demande_id.lower() in quitting_words:
+            print("Vous avez quitté le programme. À bientôt !")
+            return None
+        
+        try:
+            code = int(demande_id)
+            if code in pins_valides:
+                print("PIN correct. Accès autorisé.\n")
+                return code  
+            else:
+                print("PIN incorrect. Réessayez.\n")
+        except:
+            print("Vous devez entrer un nombre pour le code PIN.\n")
+
+def demarrer_distributeur():
+    message_de_bienvenue()
+    pin = entrer_code_pin()
+    if pin is not None:
+        print("Bienvenue dans votre compte bancaire !")
+    else:
+        print("Fin du programme.")
+
+demarrer_distributeur()
