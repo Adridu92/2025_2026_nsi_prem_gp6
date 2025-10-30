7d1a99ec7a83e71ff067cebb51c9671ef60eae7c
Estesan <estebansolanille169@gmail.com>
2025-10-30 15:05:52 +0100

    Update databank.py

--- databank.py
+++ databank.py
@@ -1,53 +1,38 @@
-1013:
-"nom" : Macaron,
-"prenom" : Manuel,
-"solde" : 999999
-"depots" : [{"montant":10000, "date":05-10-2025}, \
-            {"montant":12000, "date":02-10-2025}]
-"retraits":[]
-1033:
-"nom" : Goataldo,
-"prenom" : Christian,
-"solde" : 10023
-"depots" : [{"montant":1300, "date":03-10-2025}, \
-            {"montant":1200, "date":02-10-2025}]
-"retraits":[{"montant":200, "date":04-09-2025}]
-1023:
-"nom" : Raisingay,
-"prenom" : Hadrien,
-"solde" : 12
-"depots" : [{"montant":1, "date":03-10-2025}, \
-            {"montant":2, "date":01-10-2025}],
-"retraits":[{"montant": 1, "date":06-10-2025}]
-1043:
-"nom" : Moisi,
-"prenom" : Leon,
-"solde" : 0
-"depots" : [{"montant":1, "date":03-10-2025}, \
-            {"montant":1, "date":02-10-2025}],
-"retraits":[{"montant":2,"date":04-10-2025}]
-1073:
-"nom" : Pêcheur,
-"prenom" : Marin,
-"solde" : 1200,
-"depots" : [{"montant":700, "date":"01-10-2025"},\
-             {"montant":600, "date":"03-10-2025"}],
-"retraits" : [{"montant":300, "date":"05-10-2025"}]
-1063:
-"nom" : clenchetasoeur,
-"prenom" : Jean,
-"solde" : 8000,
-"depots" : [{"montant":500, "date":"02-10-2025"}],
-"retraits" : [{"montant":150, "date":"04-10-2025"}]
-1093:
-"nom" : Boulard,
-"prenom" : Alban,
-"solde" : 1500,
-"depots" : [{"montant":100, "date":"03-10-2025"}],
-"retraits" : [{"montant":75, "date":"04-10-2025"}]
-1193:
-"nom" : jinping,
-"prenom" : Xi,
-"solde" : 240000000,
-"depots" : [{"montant":100000, "date":"03-10-2025"}],
-"retraits" : [{"montant":75000, "date":"04-10-2025"}]
+clients = [
+    ["1013", "Manuel", "Macaron", 999999,
+        [10000, "2025-10-05", 12000, "2025-10-02"],
+        []
+    ],
+    ["1033", "Christian", "Goataldo", 10023,
+        [1300, "2025-10-03", 1200, "2025-10-02"],
+        [200, "2025-09-04"]
+    ],
+    ["1023", "Hadrien", "Raisingay", 12,
+        [1, "2025-10-03", 2, "2025-10-01"],
+        [1, "2025-10-06"]
+    ],
+    ["1043", "Leon", "Moisi", 0,
+        [1, "2025-10-03", 1, "2025-10-02"],
+        [2, "2025-10-04"]
+    ],
+    ["1073", "Marin", "Pêcheur", 1200,
+        [700, "2025-10-01", 600, "2025-10-03"],
+        [300, "2025-10-05"]
+    ],
+    ["1063", "Jean", "clenchetasoeur", 8000,
+        [500, "2025-10-02"],
+        [150, "2025-10-04"]
+    ],
+    ["1093", "Alban", "Boulard", 1500,
+        [100, "2025-10-03"],
+        [75, "2025-10-04"]
+    ],
+    ["1193", "Xi", "jinping", 240000000,
+        [100000, "2025-10-03"],
+        [75000, "2025-10-04"]
+    ]
+]
+
+quitting_words = ["quit", "ciao", "byebye", "au revoir", "adios"]
+
+pins_valides = [1013, 1023, 1033, 1043, 1063, 1073, 1093, 1193]
